7142764ad4aad3ffd716ee09e1393320a2fc14d0
RaymondJohnson02 <johnson.raymond2002gmail.com>
2022-01-28 16:03:02 +0700

    delete file interfacehewan.java

--- src/packageHewan/InterfaceHewan.java
+++
@@ -1,9 +0,0 @@
-package packageHewan;
-
-public interface InterfaceHewan {
-	public final String name = "Ini hewan";
-	public void bergerak();
-	default public void test() {
-		System.out.println("test");
-	}
-}
